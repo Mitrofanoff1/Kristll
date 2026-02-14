d4f8622d78aa7a2c0e93ccdc6ffe64781aaa3785
Mitrofanoff1 <mitrofanoff97@gmail.com>
2026-02-14 18:48:44 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import Hero from "@/components/Hero";
 import Block3 from "@/components/Block3";
 import Price from "@/components/Price";
-import Results from "@/components/Results"; // Импорт
+import Results from "@/components/Results";
+import LaserInfo from "@/components/LaserInfo"; // Добавь этот импорт
 import Block2 from "@/components/Block2"; 
 
 export default function Home() {
@@ -10,7 +11,8 @@ export default function Home() {
       <Hero />
       <Block3 />
       <Price />
-      <Results /> {/* Блок результатов */}
+      <Results />
+      <LaserInfo /> {/* Информация о лазере */}
       
       {/* <Block2 /> */}
     </main>
