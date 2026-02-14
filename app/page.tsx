f6cfcb7c8c6e458ce1c4cde43d8c00b9cf9aef91
Mitrofanoff1 <mitrofanoff97@gmail.com>
2026-02-14 16:48:10 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,16 @@
 import Hero from "@/components/Hero";
+import Block3 from "@/components/Block3"; // Импортируем новый блок
 import Block2 from "@/components/Block2"; 
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-white">
-      {/* Главный экран */}
       <Hero />
       
-      {/* 
-         Здесь мы будем добавлять новые блоки (Block3, Block4 и т.д.) 
-      */}
+      {/* Теперь Block3 идет сразу вторым */}
+      <Block3 />
 
-      {/* Пока скрываем Block2, чтобы поставить его позже в конец */}
+      {/* Block2 пока скрыт или будет в самом конце */}
       {/* <Block2 /> */}
     </main>
   );
