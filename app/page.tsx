26d2090fef04fd7e4b411379c1ae5ea93f4e7c0c
Mitrofanoff1 <mitrofanoff97@gmail.com>
2026-02-14 17:39:20 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,15 @@
 import Hero from "@/components/Hero";
-import Block3 from "@/components/Block3"; // Импортируем новый блок
+import Block3 from "@/components/Block3";
+import Price from "@/components/Price"; // Импортируем прайс
 import Block2 from "@/components/Block2"; 
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-white">
       <Hero />
-      
-      {/* Теперь Block3 идет сразу вторым */}
       <Block3 />
-
-      {/* Block2 пока скрыт или будет в самом конце */}
+      <Price /> {/* Ставим прайс третьим блоком */}
+      
       {/* <Block2 /> */}
     </main>
   );
